a5bb400480c2d191867f7a688fe302cff9a222ea
augusstt06 <augusstt06@gmail.com>
2024-03-05 17:25:27 +0900

    lint test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
+
+import reactLogo from '@/assets/react.svg';
+
+import viteLogo from '../public/vite.svg';
+
+
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
   // const a = 1;
+  useEffect(() => {}, []);
   return (
     <>
       <div>
