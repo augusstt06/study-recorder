9f4ee1fb030893f4ec2a22ab1fcf3c7e80c3e3d7
augusstt06 <augusstt06@gmail.com>
2024-03-18 15:24:45 +0900

    add : get specific category data

--- backend/app.ts
+++ backend/app.ts
@@ -1,29 +1,45 @@
 import { Request, Response } from "express";
+import { CORS, SERVER_ERR_CODE, SERVER_ERR_MSG, SERVER_PORT } from "./constant";
 import { ResponseDBList } from "./types";
 
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT: number = SERVER_PORT;
 
 const mysql = require("mysql2");
 const dbConfig = require("./config/db.config");
 const pool = mysql.createPool(dbConfig).promise();
 
 app.get("/", async (req: Request, res: Response) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(CORS, process.env.CLIENT_URL);
   try {
     const connection = await pool.getConnection();
-    const sql = "select * from records";
-    const [rows] = await connection.query(sql);
+    const sql: string = "SELECT * FROM records";
+    const [rows]: ResponseDBList = await connection.query(sql);
     res.send(rows);
     connection.release();
   } catch (err) {
-    res.status(500).send("Internal Server Error");
+    res.status(SERVER_ERR_CODE).send(SERVER_ERR_MSG);
+  }
+});
+
+app.get("/:category", async (req: Request, res: Response) => {
+  res.header(CORS, process.env.CLIENT_URL);
+  try {
+    const category: string = req.params.category;
+    const sql: string = `SELECT * FROM records WHERE category = '#${category}'`;
+    const connection = await pool.getConnection();
+    const [rows]: ResponseDBList = await connection.query(sql);
+    res.send(rows);
+    connection.release();
+  } catch (err) {
+    console.log(err);
+    res.status(SERVER_ERR_CODE).send(SERVER_ERR_MSG);
   }
 });
 app.listen(PORT, () => {
-  console.log("**----------------------------------**");
+  console.log("--------------------------------------");
   console.log("====      Server is Running!      ====");
-  console.log("**----------------------------------**");
+  console.log("--------------------------------------");
 });
---
+++ backend/constant/index.ts
@@ -0,0 +1,7 @@
+// Error
+export const SERVER_ERR_CODE = 500;
+export const SERVER_ERR_MSG = "Internal Server Error";
+
+// others
+export const SERVER_PORT = 4000;
+export const CORS = "Access-Control-Allow-Origin";
